77389d60716707afedd3fd9d7fc4cad92f4d7677
geekbrains <info@geekbrains.ru>
2021-08-30 20:52:18 +0300

    Homework 8 is done

---
+++ src/Home8Main.java
@@ -0,0 +1,5 @@
+public class Home8Main {
+    public static void main(String[] args) {
+        new MyWindow();
+    }
+}
---
+++ src/MyWindow.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow extends JFrame {
+    public MyWindow() {
+        setLocation(650, 450);
+        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+
+        JButton btn1 = new JButton("Push the button!");
+        JButton btn2 = new JButton("Exit");
+
+        ActionListener actionListener = new CloseActionListener();
+        btn2.addActionListener(actionListener);
+
+        panel.add(btn1);
+        panel.add(btn2);
+
+        add(panel, BorderLayout.SOUTH);
+
+        setVisible(true);
+
+    }
+}
